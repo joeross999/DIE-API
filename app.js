79bed618094a7ef0fce7e94323da619908c03ff3
Joe Ross <joeross999@gmail.com>
2018-08-10 22:18:13 -0400

    Set up Express Sessions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var favicon = require('serve-favicon');
+var session = require('express-session')
 
 var port = 3000
 
@@ -19,17 +20,27 @@ app.use(bodyParser.urlencoded({
   extended: true
 })); 
 
+// Session Setup
+app.set('trust proxy', 1)
+app.use(session({
+  secret: 'SessionSecret...',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
 // ROUTES
 app.get('/', function(req, res){
   res.render('index');
 });
 let testUser = "TESTUSER"
 app.post('/init', function(req, res){
-  main.init(req.body, res, testUser);
+  main.init(req.body, res, req.sessionID);
+  console.log(req.sessionID)
 });
 
 app.get('/refresh', function(req, res){
-  main.frame(res, testUser);
+  main.frame(res, req.sessionID);
 });
 
 
